92d6f602057ffb18a352ee2215ac5928597afa18
susanngu <susanngu@stud.ntnu.no>
2013-10-21 16:04:19 -0700

    hmm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Susannejulie::Application.config.secret_key_base = 'f584f4e00b0af9c0201d813fd21deaf3b86cb9be1352c124b6bdf715bc117063d497565bf51eef7026e0cbe8a38a5bd69d61c900697b3a1201354d097f0e2a91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Susannejulie::Application.config.session_store :cookie_store, key: '_susannejulie_session'
